f3ed538a58d576d253c7ff1201064fba82f5d746
lap2ka <83134094+lap2ka@users.noreply.github.com>
2026-05-16 12:00:22 +0200

    Fabric 1.20.1 Port

--- src/main/java/dev/lapt/nowheel/NowheelMod.java
+++ src/main/java/dev/lapt/nowheel/NowheelMod.java
@@ -4,17 +4,17 @@ import com.mojang.logging.LogUtils;
 import dev.engine_room.flywheel.api.visualization.VisualizationManager;
 import dev.lapt.nowheel.cull.CullTransitions;
 import dev.lapt.nowheel.flywheel.FlywheelVisualToggleListener;
+import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
 
-@Mod(value = NowheelMod.MODID, dist = net.neoforged.api.distmarker.Dist.CLIENT)
-public class NowheelMod {
+public class NowheelMod implements ClientModInitializer {
     public static final String MODID = "nowheel";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public NowheelMod() {
+    @Override
+    public void onInitializeClient() {
         CullTransitions.BE.register(new FlywheelVisualToggleListener<>(
             BlockEntity::getLevel, VisualizationManager::blockEntities));
         CullTransitions.ENTITY.register(new FlywheelVisualToggleListener<>(
