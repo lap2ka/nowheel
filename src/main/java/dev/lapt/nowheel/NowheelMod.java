ef443cb998567157f48ce3dc7f0c56911820e625
lap2ka <83134094+lap2ka@users.noreply.github.com>
2026-05-18 07:27:16 +0200

    NEVER trust IDEA

--- src/main/java/dev/lapt/nowheel/NowheelMod.java
+++ src/main/java/dev/lapt/nowheel/NowheelMod.java
@@ -20,10 +20,11 @@ public class NowheelMod {
     public static final String MODID = "nowheel";
     public static final Logger LOGGER = LogUtils.getLogger();
 
+    @SuppressWarnings("Convert2MethodRef")
     public NowheelMod() {
         if (FMLEnvironment.dist != Dist.CLIENT) return;
-        CullTransitions.BE.register(new FlywheelVisualToggleListener<>(BlockEntity::getLevel, VisualizationManager::blockEntities));
-        CullTransitions.ENTITY.register(new FlywheelVisualToggleListener<>(Entity::level, VisualizationManager::entities));
+        CullTransitions.BE.register(new FlywheelVisualToggleListener<>(be -> be.getLevel(), VisualizationManager::blockEntities));
+        CullTransitions.ENTITY.register(new FlywheelVisualToggleListener<>(e -> e.level(), VisualizationManager::entities));
 
         if (ModList.get().isLoaded("cloth_config")) {
             ModLoadingContext.get().registerExtensionPoint(
