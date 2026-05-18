9675fd48e4d652967f1aca52ce6ff91ea3b2c706
lap2ka <83134094+lap2ka@users.noreply.github.com>
2026-05-18 07:27:16 +0200

    NEVER trust IDEA (parity) [no ci]

--- src/main/java/dev/lapt/nowheel/NowheelMod.java
+++ src/main/java/dev/lapt/nowheel/NowheelMod.java
@@ -26,9 +26,10 @@ public class NowheelMod {
 
     private boolean sableHooked = false;
 
+    @SuppressWarnings("Convert2MethodRef")
     public NowheelMod(ModContainer container) {
-        CullTransitions.BE.register(new FlywheelVisualToggleListener<>(BlockEntity::getLevel, VisualizationManager::blockEntities));
-        CullTransitions.ENTITY.register(new FlywheelVisualToggleListener<>(Entity::level, VisualizationManager::entities));
+        CullTransitions.BE.register(new FlywheelVisualToggleListener<>(be -> be.getLevel(), VisualizationManager::blockEntities));
+        CullTransitions.ENTITY.register(new FlywheelVisualToggleListener<>(e -> e.level(), VisualizationManager::entities));
 
         if (ModList.get().isLoaded("cloth_config")) {
             Supplier<IConfigScreenFactory> configScreen = () -> (mc, previousScreen) -> NowheelConfigScreen.create(previousScreen);
