c941aaf6f92cfa517c7bceaf0661f90951530784
lap2ka <83134094+lap2ka@users.noreply.github.com>
2026-05-19 16:02:48 +0200

    Rework how overriding config works to not persist these values in config, set override as false by default

--- src/main/java/dev/lapt/nowheel/mixin/EntityCullingConfigOverrideMixin.java
+++ src/main/java/dev/lapt/nowheel/mixin/EntityCullingConfigOverrideMixin.java
@@ -1,9 +1,13 @@
 package dev.lapt.nowheel.mixin;
 
-import dev.lapt.nowheel.NowheelMod;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import dev.lapt.nowheel.config.NowheelConfig;
 import dev.tr7zw.entityculling.EntityCullingModBase;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -11,21 +15,35 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(value = EntityCullingModBase.class, remap = false)
 public class EntityCullingConfigOverrideMixin {
 
-    @Inject(
+    @Unique
+    private boolean nowheel$overridenWhitelist = false;
+
+    @Unique
+    private static final ResourceLocation ROPE_PULLEY = ResourceLocation.parse("create:rope_pulley");
+    @Unique
+    private static final ResourceLocation HOSE_PULLEY = ResourceLocation.parse("create:hose_pulley");
+
+    @ModifyExpressionValue(
         method = "onInitialize", at = @At(
-        value = "INVOKE", target = "Ldev/tr7zw/entityculling/versionless/EntityCullingVersionlessBase;onInitialize()V", shift = At.Shift.AFTER
+        value = "FIELD", target = "Ldev/tr7zw/entityculling/versionless/Config;tracingDistance:I", opcode = Opcodes.GETFIELD
     )
     )
-    private void nowheel$overrideCullingLimits(CallbackInfo ci) {
+    private int nowheel$overrideCullingLimits(int original) {
         if (!NowheelConfig.get().overrideEntityCulling) {
+            return original;
+        }
+        return Math.max(original, NowheelConfig.TRACING_DISTANCE_OVERRIDE);
+    }
+
+    @Inject(method = "clientTick", at = @At("TAIL"))
+    private void nowheel$overrideWhitelist(CallbackInfo ci) {
+        if (nowheel$overridenWhitelist || !NowheelConfig.get().overrideEntityCulling) {
             return;
         }
         EntityCullingModBase ec = (EntityCullingModBase) (Object) this;
-        // Entity Culling assumes that the entites/block entities work like in vanilla, which is just not how it is in Create
-        ec.config.tracingDistance = Math.max(ec.config.tracingDistance, 384);
-        ec.config.hitboxLimit = Math.max(ec.config.hitboxLimit, 1000);
-        // We handle these, so let them be culled
-        ec.config.blockEntityWhitelist.remove("create:rope_pulley");
-        ec.config.blockEntityWhitelist.remove("create:hose_pulley");
+
+        ec.blockEntityWhitelist.remove(BuiltInRegistries.BLOCK_ENTITY_TYPE.get(ROPE_PULLEY));
+        ec.blockEntityWhitelist.remove(BuiltInRegistries.BLOCK_ENTITY_TYPE.get(HOSE_PULLEY));
+        nowheel$overridenWhitelist = true;
     }
 }
