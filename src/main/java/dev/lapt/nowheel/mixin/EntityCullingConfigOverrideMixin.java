3bf51da34188f406503338db21ad3476c2180912
lap2ka <83134094+lap2ka@users.noreply.github.com>
2026-05-22 10:03:42 +0200

    Fix pretty much every broken BE

--- src/main/java/dev/lapt/nowheel/mixin/EntityCullingConfigOverrideMixin.java
+++ src/main/java/dev/lapt/nowheel/mixin/EntityCullingConfigOverrideMixin.java
@@ -62,7 +62,7 @@ public class EntityCullingConfigOverrideMixin {
             blockEntityWhitelist.remove(BuiltInRegistries.BLOCK_ENTITY_TYPE.get(HOSE_PULLEY));
             NowheelMod.LOGGER.info("Overriden Entity Culling whitelist");
         } catch (final Throwable t) {
-            NowheelMod.LOGGER.error("[nowheel] Error overriding whitelist, what the helly: ", t);
+            NowheelMod.LOGGER.error("Error overriding whitelist, what the helly: ", t);
         }
         nowheel$overridenWhitelist = true;
     }
